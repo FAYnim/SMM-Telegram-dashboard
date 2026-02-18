1d75dd76c9333f11866f6e8129b9c84b29794e8a
FAYnim <yuana6898@gmail.com>
2026-02-18 13:16:36 +0700

    feat: implement authentication token validation and redirect in multiple JS files

--- src/js/users.js
+++ src/js/users.js
@@ -1,6 +1,31 @@
 import { CookieManager } from "./auth-middleware.js";
 
 $(document).ready(function() {
+
+    const auth_token = CookieManager.get("auth_token");
+    if(!auth_token) {
+        window.location.href = "login.php";
+    } else {
+        $.ajax({
+            url: 'src/api/auth-middleware.php',
+            type: 'POST',
+            dataType: 'json',
+            data: {'auth_token': auth_token},
+            success: (response) => {
+                if(response.returncode == 200) {
+                    console.log("User Logged In");
+                } else {
+                    window.location.href = "login.php";
+                }
+            },
+            error: (xhr, status, error) => {
+                console.log("Status: ", status);
+                console.log("Login failed: ", xhr.responseText);
+                console.log("Error: ", error);
+            }
+        });
+    }
+
     $('#sidebarToggle').on('click', function() {
         $('#sidebar').toggleClass('collapsed show');
         $('#mainContent').toggleClass('expanded');
