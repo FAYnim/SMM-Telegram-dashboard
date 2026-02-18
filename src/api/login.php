72127ea54ca7ee17eef300dbfbacb582a366683b
FAYnim <yuana6898@gmail.com>
2026-02-18 11:02:48 +0700

    fix: correct Content-Type header and implement user authentication logic

--- src/api/login.php
+++ src/api/login.php
@@ -1,8 +1,38 @@
 <?php
-header('contentType: application/json');
+header('Content-Type: application/json');
 
-$r = array(
-    "returncode" => 200
-);
+require_once __DIR__.'/../include/db.php';
+$db = new Database();
+$conn = $db->getConnection();
+
+$user = $_POST['username'] ?? ''; 
+$pass = $_POST['password'] ?? '';
+
+if(empty($user) || empty($pass)) {
+    http_response_code(400);
+    echo json_encode(["error" => "Fill all the inputs!"]);
+    exit;
+}
+
+$user = trim($user);
+$user = htmlspecialchars($user, ENT_QUOTES, 'UTF-8');
+$pass = trim($pass);
+$pass = htmlspecialchars($pass, ENT_QUOTES, 'UTF-8');
+
+$sql = $db->select('smm_admins', '*', ["username" => "$user", "chatid" => "$pass"]);
+if ($sql) {
+    $result = $sql[0];
+
+    $token = bin2hex(random_bytes(16));
+} else {
+    http_response_code(401);
+    echo json_encode(["error" => "user not found"]);
+    exit;
+}
+
+$r = [
+    "returncode" => 200,
+    "token" => $token
+];
 echo json_encode($r);
 ?>
