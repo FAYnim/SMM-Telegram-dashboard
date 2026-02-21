65de8897afb0e37d13598d71203f3c7e19c7d213
FAYnim <yuana6898@gmail.com>
2026-02-21 17:27:42 +0700

    feat: Add JWT login endpoint and authentication middleware.

--- src/api/auth-middleware.php
+++ src/api/auth-middleware.php
@@ -5,31 +5,42 @@ require_once __DIR__.'/../include/db.php';
 $db = new Database();
 $conn = $db->getConnection();
 
-$auth_token = $_POST['auth_token'] ?? '';
+$jwt = $_COOKIE['auth_token'];
 
-if(empty($auth_token)){
+if(empty($jwt)){
     http_response_code(400);
     echo json_encode(["error" => "Auth token needed!"]);
     exit;
 }
 
-$sql = $db->select("smm_admin_sessions", "*", ["token" => $auth_token]);
-if($sql) {
-    $result = $sql[0];
-    $expired = $result["expired_at"];
+// JWT Practice
+list($header, $payload, $signature) = explode('.', $jwt);
 
-    if(strtotime($expired) < time()) {
-        http_response_code(401);
-        echo json_encode(["error" => "Auth token expired"]);
-        exit;
-    }
-} else {
+// cek signature
+$expectedSignature = hash_hmac('sha256', $header.'.'.$payload, 'secret_key_faydev');
+
+if($signature !== $expectedSignature) {
     http_response_code(401);
-    echo json_encode(["error" => "Auth token not found"]);
+    echo json_encode(["error" => "Auth token invalid"]);
+    exit;
+}
+
+// decode payload
+$payload = json_decode(base64_decode($payload), true);
+$user = $payload['user'];
+$loggedIn = $payload['loggedIn'];
+
+// cek expired
+if(time() > $payload['exp']) {
+    http_response_code(401);
+    echo json_encode(["error" => "Auth token expired"]);
+    exit;
 }
 
 $r = [
     "returncode" => 200,
+    "user" => $user,
+    "loggedIn" => $loggedIn
 ];
 echo json_encode($r);
 
--- src/api/login.php
+++ src/api/login.php
@@ -19,24 +19,45 @@ $user = htmlspecialchars($user, ENT_QUOTES, 'UTF-8');
 $pass = trim($pass);
 $pass = htmlspecialchars($pass, ENT_QUOTES, 'UTF-8');
 
+// JWT Practice
+$header = json_encode([
+    'alg' => 'HS256',
+    'typ' => 'JWT'
+]);
+
 $sql = $db->select('smm_admins', '*', ["username" => "$user", "chatid" => "$pass"]);
 if ($sql) {
     $result = $sql[0];
 
-    $token = bin2hex(random_bytes(16));
+    // $token = bin2hex(random_bytes(16));
 
-    // cek token
-    $sql = $db->select('smm_admin_sessions', '*', ["username" => "$user"]);
-    if($sql) {
-        $db->update('smm_admin_sessions', ["token" => $token, "expired_at" => date("Y-m-d H:i:s", strtotime("+7 days"))], ["username" => "$user"]);
-    } else {
-        $data = [
-            "token" => $token,
-            "username" => $user,
-            "expired_at" => date("Y-m-d H:i:s", strtotime("+7 days"))
-        ];
-        $db->insert('smm_admin_sessions', $data);
-    }
+    // // cek token
+    // $sql = $db->select('smm_admin_sessions', '*', ["username" => "$user"]);
+    // if($sql) {
+    //     $db->update('smm_admin_sessions', ["token" => $token, "expired_at" => date("Y-m-d H:i:s", strtotime("+7 days"))], ["username" => "$user"]);
+    // } else {
+    //     $data = [
+    //         "token" => $token,
+    //         "username" => $user,
+    //         "expired_at" => date("Y-m-d H:i:s", strtotime("+7 days"))
+    //     ];
+    //     $db->insert('smm_admin_sessions', $data);
+    // }
+
+    // JWT Practice
+    $payload = json_encode([
+        'user' => $user,
+        'loggedIn' => true,
+        'exp' => time() + 60 * 60 * 24 * 7
+    ]);
+
+    // Signature (tanda tangan)
+    $base64Header = base64_encode($header);
+    $base64Payload = base64_encode($payload);
+
+    $signature = hash_hmac('sha256', $base64Header.'.'.$base64Payload, 'secret_key_faydev');
+
+    $jwt = $base64Header.'.'.$base64Payload.'.'.$signature;
 } else {
     http_response_code(401);
     echo json_encode(["error" => "user not found"]);
@@ -45,7 +66,7 @@ if ($sql) {
 
 $r = [
     "returncode" => 200,
-    "token" => $token
+    "token" => $jwt
 ];
 echo json_encode($r);
 ?>
